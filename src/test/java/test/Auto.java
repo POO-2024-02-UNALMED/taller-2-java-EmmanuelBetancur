8c605c5b6bb3d1e259b7023a8d660ba0765b0bbb
EmmanuelBetancur <ebetancuru@unal.edu.co>
2024-11-10 20:33:38 -0500

    Copiadas las clases a la carpeta Test

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,40 @@
+package test;
+
+public class Auto {
+
+    //atributos
+    String modelo;
+    int precio;
+    Asiento asientos[];
+    String marca;
+    Motor motor;
+    int registro;
+    static String cantidadCreados;
+
+    //El metodo cantidadAsientos recibira un objeto de tipo auto y devolvera el numero de
+    //objetos de tipo asiento que hagan parte de el array asientos
+
+    int cantidadAsientos(Auto auto){
+
+        int nasientos; 
+        nasientos = size(this.asientos);
+        return nasientos; 
+
+    }
+
+    //El metodo verificarIntegridad comparara el numero de registro de los asientos, del motor y
+    //del auto para comprobar si son o no originales.
+
+    String verificarIntegridad(Auto auto){
+
+        if (this.registro != (motor.registro && asientos.registro)) {
+
+            return "Las piezas no son originales";
+
+        } else {
+            return "Auto original";
+        }
+
+    }
+
+}
